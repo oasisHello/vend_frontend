e4eb45e98bb48f289a824e239bf4a9d17abb45ab
oasis <gongstats@gmail.com>
2025-01-04 14:23:33 +0900

    feature: add employee management

---
+++ src/api/manage/emp.js
@@ -0,0 +1,44 @@
+import request from '@/utils/request'
+
+// 查询Employee List列表
+export function listEmp(query) {
+  return request({
+    url: '/manage/emp/list',
+    method: 'get',
+    params: query
+  })
+}
+
+// 查询Employee List详细
+export function getEmp(id) {
+  return request({
+    url: '/manage/emp/' + id,
+    method: 'get'
+  })
+}
+
+// 新增Employee List
+export function addEmp(data) {
+  return request({
+    url: '/manage/emp',
+    method: 'post',
+    data: data
+  })
+}
+
+// 修改Employee List
+export function updateEmp(data) {
+  return request({
+    url: '/manage/emp',
+    method: 'put',
+    data: data
+  })
+}
+
+// 删除Employee List
+export function delEmp(id) {
+  return request({
+    url: '/manage/emp/' + id,
+    method: 'delete'
+  })
+}
