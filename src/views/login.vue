2c48cbdbf9309c7f6c00ab4ea4f67e7bb8933de4
oasishalo <gongstats@gmail.com>
2025-01-15 13:54:10 +0900

    feature: enhance the UI color

--- src/views/login.vue
+++ src/views/login.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="login">
     <el-form ref="loginRef" :model="loginForm" :rules="loginRules" class="login-form">
-      <h3 class="title">Vending Entrance</h3>
+      <h3 class="title">ENTRANCE</h3>
       <el-form-item prop="username">
         <el-input v-model="loginForm.username" type="text" size="large" auto-complete="off" placeholder="账号">
           <template #prefix><svg-icon icon-class="user" class="el-input__icon input-icon" /></template>
@@ -151,7 +151,7 @@ getCookie();
 .title {
   margin: 0px auto 30px auto;
   text-align: center;
-  color: #264653;
+  color: #FF7043;
   /* Deep teal for contrast */
   font-family: 'Arial', sans-serif;
   /* Sets the font type */
