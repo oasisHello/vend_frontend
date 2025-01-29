d22186b150014dbe71b56b2a5f2d222fc2ab9c99
oasis <gongstats@gmail.com>
2025-01-29 20:09:56 +0900

    feature: support english in swagger

--- src/views/tool/swagger/index.vue
+++ src/views/tool/swagger/index.vue
@@ -5,5 +5,5 @@
 <script setup>
 import iFrame from '@/components/iFrame'
 
-const url = ref(import.meta.env.VITE_APP_BASE_API + "/doc.html#/home/en-US");
+const url = ref(import.meta.env.VITE_APP_BASE_API + "/doc.html#/home/en-US");//support English,https://doc.xiaominfo.com/docs/features/i18n
 </script>
