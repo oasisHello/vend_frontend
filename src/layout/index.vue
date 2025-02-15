e4eff9bc307a6023658edf877db0d40d0bb98e43
oasishalo <gongstats@gmail.com>
2025-02-15 09:03:05 +0900

    comment on layout view

--- src/layout/index.vue
+++ src/layout/index.vue
@@ -1,12 +1,15 @@
 <template>
   <div :class="classObj" class="app-wrapper" :style="{ '--current-color': theme }">
     <div v-if="device === 'mobile' && sidebar.opened" class="drawer-bg" @click="handleClickOutside" />
+    <!--side bar-->
     <sidebar v-if="!sidebar.hide" class="sidebar-container" />
     <div :class="{ hasTagsView: needTagsView, sidebarHide: sidebar.hide }" class="main-container">
       <div :class="{ 'fixed-header': fixedHeader }">
+        <!-- navigation-->
         <navbar @setLayout="setLayout" />
         <tags-view v-if="needTagsView" />
       </div>
+      <!-- main -->
       <app-main />
       <settings ref="settingRef" />
     </div>
